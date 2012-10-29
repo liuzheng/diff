0bfcb6d38f111987b5ba56f07f257bd09e1f1ec3
liuzheng712 <liuzheng712@gmail.com>
2012-10-29 20:36:16 +0800

    it works

--- main.m
+++ main.m
@@ -1,16 +1,28 @@
-
 % get list
 [filename,filepath]=uigetfile(...
-    {'*.txt','Subripper (*.txt)';...
+    {'*.txt','(*.txt)';...
     '*.*','All Files (*.*)'}); 
 a=cat(2,filepath,filename);
 
 
 % get list
 [filename,filepath]=uigetfile(...
-    {'*.txt','Subripper (*.txt)';...
+    {'*.txt','(*.txt)';...
     '*.*','All Files (*.*)'}); 
 b=cat(2,filepath,filename);
 c=input('c=');
 d=input('d=');
-do(a,b,c,d)
+aa=textread(a,'%s');
+bb=textread(b,'%s');
+
+r=do(aa,bb,c,d);
+[filename ,filepath]=uiputfile({'*.txt','(*.txt)'},'±£´æ');
+e=cat(2,filepath,filename);
+fid=fopen(e,'w');
+[n m]=size(r);
+for i=1:n
+    for j=1:m
+        fprintf(fid,'%s\n',r{i,j});
+    end
+end
+fclose(fid);
